6f01c4d4410261f1ed8c6e287d309d4870663734
Hatano Yuuta <hatano.yuuta@gmail.com>
2019-12-28 13:57:57 +0900

    6-8まで追記

---
+++ ６章：APIとPWAを連携させよう/6-8：マニフェストファイルを完成させよう/sw.js
@@ -0,0 +1,28 @@
+// キャッシュ名とキャッシュファイルの指定
+var CACHE_NAME = 'simple-pwa-caches';
+
+var urlsToCache = [
+  './css/style.css',
+  './page1.html',
+  './page2.html',
+  './images/loading.gif'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response ? response : fetch(event.request);
+	})
+  );
+});
